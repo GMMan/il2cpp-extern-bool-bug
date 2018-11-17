dea20e87069e2f56fee4cd330f06fa0c40e4ff5c
Yukai Li <yukaili.geek@gmail.com>
2018-11-17 12:18:34 -0700

    Add Unity project

---
+++ il2cpp-test/Assets/Scripts/TestScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour {
+    Text target;
+
+	// Use this for initialization
+	void Start () {
+        target = GetComponent<Text>();
+		if (target != null)
+        {
+            target.text = NativeImport.TestFunc().ToString();
+        }
+	}
+}
